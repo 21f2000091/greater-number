add31a62ebc14e71d8538c1d65dbfd41cce86aba
21f2000091 <119449326+21f2000091@users.noreply.github.com>
2022-11-30 01:04:25 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-num1=st.number_input("Enter first number")
+num=st.number_input("Enter first number")
 num2=st.number_input("Enter second number")
 num3=st.number_input("Enter third number")
 m=max(num1,num2,num3)
